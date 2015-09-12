be17b74f7bf0ea326a8d949830e3a35cb4df00b5
Shahriar Alam <imsas@users.noreply.github.com>
2015-09-12 18:13:32 +0600

    Fixed Visulal editor issue in WordPress 4.3

--- modules/richtext/richtext.php
+++ modules/richtext/richtext.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Rich Text
   Plugin URI: #
-  Description: Rich Text Module For MiniMax
+  Description: Visual Editor module For MiniMax
   Author: Shaon
   Version: 1.6
   Author URI: #
@@ -10,129 +10,144 @@
 
 if (!class_exists('MiniMax_RichText')) {
 
-    class MiniMax_RichText extends WP_Widget {
+	add_action('widgets_init', 'register_richtext');
+	function register_richtext(){
+		register_widget('MiniMax_RichText');
+	}
 
-        function __construct() {
-            global $pagenow;
-            parent::WP_Widget(/* Base ID */'MiniMax_RichText', /* Name */ 'Rich Text', array('description' => 'Rich Text Editor'));
-        }
+	include(dirname(__FILE__) . '/dynamic-tinymce.php');
 
-        /** @see WP_Widget::widget */
-        function widget($args, $instance) {
-            extract($args);
-            $cssclass = $instance['cssclass'];
-            $content = $instance['content'];
+	class MiniMax_RichText extends WP_Widget {
 
-            if (!empty($content) && $cssclass != '') {
-                echo "<div class='{$cssclass}'>" . $content . "</div>";
-            } else if (!empty($content) && $cssclass == '') {
-                echo $content;
-            }
-         
-        }
+		public function __construct() {
 
-        function update($new_instance, $old_instance) {
-            $instance = $new_instance;
+			parent::__construct(
+				'MiniMax_RichText', // Base ID
+				'Rich Text', // Name
+				array('description' => 'MiniMax Visual Editor Module'), // Args
+				array('width' => 600, 'height' => 550)
+			);
+                        wp_enqueue_media();
+		}
 
-            return $instance;
-        }
+		public function widget($args, $instance) {
+			extract($args);
 
-        
-        function form($instance) {
+			if (!$instance['cssclass'] && !$instance['content']) return;
 
-            if ($instance) {
-                extract($instance);
-            } else {
-                $content = "";
-                $cssclass = "";
-            }
-            ?>
-           
+			$cssclass = isset($instance['cssclass']) ? $instance['cssclass'] : '';
+			$text = apply_filters('widget_text', $instance['content'], $instance);
 
-            <div id="tabpane">
+			echo $before_widget; ?>
+                        
+                            <?php if ($text) : ?><div class="<?php echo $cssclass; ?>"><?php echo $text; ?></div><?php endif; ?>
+			
+                        <?php echo $after_widget;
+		}
 
-            <p>
+		public function form($instance) {
+			$instance = wp_parse_args((array)$instance, array('cssclass' => '', 'content' => ''));
+			$id = uniqid('e'); 
+                        dynamic_tinymce(array(
+				'id' 	=> $id,
+				'name' 	=> $this->get_field_name('content'),
+				'value' => $instance['content'],
+				'rows' 	=> 15
+			)); ?>
+                                
+			<p>
+                            <label for="<?php echo $this->get_field_id('cssclass'); ?>"><?php _e('CSS Class Name:'); ?></label>
+                            <input class="widefat" id="<?php echo $this->get_field_id('cssclass'); ?>" name="<?php echo $this->get_field_name('cssclass'); ?>" type="text" value="<?php echo esc_attr($instance['cssclass']); ?>">
+			</p>    
 
-            <?php
-            $id = uniqid('e');
-            
-            wp_editor( $content, $id, array('editor_class' => 'wp-editor',
-                                            'textarea_name' => $this->get_field_name('content'),
-                                            'textarea_rows' => 15,
-                                            'media_buttons' => true
-                                            )
-                     );
-            ?>
-                 
-                <script type="text/javascript">
-                    
-                    tinymce.init({
-                        selector: "#<?php echo $id;?>",
-                        relative_urls: false,
-                        plugins: "wplink"
-                    });
-                    
-                    function load_mceeditor($element) {
-                        var textfield_id = $element.attr("id");
-                        //tinymce.execCommand('mceAddControl',false,textfield_id );
-                        window.tinyMCEPreInit.mceInit[textfield_id] = _.extend({}, tinyMCEPreInit.mceInit['content']);
+		<?php
+		}
 
-                        if(_.isUndefined(tinyMCEPreInit.qtInit[textfield_id])) {
-                            window.tinyMCEPreInit.qtInit[textfield_id] = _.extend({}, tinyMCEPreInit.qtInit['replycontent'], {id: textfield_id})
-                        }
+		public function update($new_instance, $old_instance) {
+			$instance = $old_instance;
+			$instance['cssclass'] = strip_tags($new_instance['cssclass']);
+			$instance['content'] = $new_instance['content'];
+			return $instance;
+		}
+	}
 
-                        quicktags( window.tinyMCEPreInit.qtInit[textfield_id] );
-                        //$element.val($content_holder.val());
-                        //tinymce.EditorManager.execCommand('mceAddControl',true, textfield_id);
-                        window.switchEditors.go(textfield_id, 'tmce');
-                    }
-                    jQuery('#tabpane .wp-editor-area').each(function(){
-                      load_mceeditor(jQuery(this));
-                    });
-                </script>
-                
-                <script type="text/javascript">
-                    jQuery(document).ready(function() {
-                        // Uploading files
-                        var file_frame;
-                        jQuery('#TB_closeWindowButton').live('click',function(){
-                            tb_remove();
-                            jQuery('#TB_window').remove();
-                        });
-                        jQuery('#iimg').live('click', function( event ){
-                            tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');return false;
-                        });
 
-                        window.send_to_editor = function(html) {
-                            tinymce.activeEditor.execCommand('mceInsertContent', false, html);
-                            //editor.insertHtml(html);
-                            //jQuery('#upload_image').val(imgurl);
-                            tb_remove();
-                            jQuery('#TB_window').remove();
-                        }
-                    });
-                </script>
-                    
-            <style>
-                .media-modal{ z-index: 999999; }
-                .mce-panel.mce-menu { z-index: 1000000 !important; }
-                #wp-link-backdrop{ z-index: 1000000 !important; }
-                #wp-link-wrap{ z-index: 99999999 !important; }
-            </style>
+	// save widget value as JSON instead of serialized array
+	add_action('pre_update_option_widget_MiniMax_RichText', 'richtext_widget_filter_update');
+	function richtext_widget_filter_update($value){
+		return json_encode($value);
+	}
+	// decode JSON when getting widget option
+	add_action('option_widget_MiniMax_RichText', 'richtext_widget_filter_option');
+	function richtext_widget_filter_option($value){
+		return is_array($value) ? $value : json_decode($value, true);
+	}
 
-            </p>
 
-            <p>
-                <label for="<?php echo $this->get_field_id('cssclass'); ?>"><?php _e('CSS Class Name:'); ?></label>
-                <input class="widefat" id="<?php echo $this->get_field_id('cssclass'); ?>" name="<?php echo $this->get_field_name('cssclass'); ?>" type="text" value="<?php echo $cssclass; ?>" />
-            </p>
+	// include our javascript
+	add_action('admin_print_footer_scripts', 'richtext_widget_footer_scripts');
+	function richtext_widget_footer_scripts() {
+		?>
+		<style>
+                    .wp-media-buttons {float: left;}
+                    .mceIframeContainer {background: #fff;}
+                    .widget-content .wp-editor-wrap {margin-bottom: 15px;}
+		</style>
+		<script>
+			(function($){
 
-            </div>
+				// parse $_GET params from url
+				function getQueryParams(qs) {
+					if (typeof qs == 'object') return qs;
 
-            <?php
-        }
+					qs = qs.split("+").join(" ");
 
-    }
+					var params = {},
+						tokens,
+						re = /[?&]?([^=]+)=([^&]*)/g;
 
-    add_action('widgets_init', create_function('', 'register_widget("MiniMax_RichText");'));
+					while (tokens = re.exec(qs)) {
+						params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+					}
+
+					return params;
+				}
+
+				// on every ajax call reinit tinyMCE
+				$(document).ajaxSuccess(function(evt, request, settings) {
+					if (!settings.data) return;
+
+					var $_GET = getQueryParams(settings.data);
+
+					// new widget added
+					if ($_GET['widget-id'] && !$_GET['delete_widget']) {
+						var widget_id = 'widget-' + $_GET['widget-id'] + '-text';
+						dynamic_tinymce_init(widget_id,['blabla','lll']);
+					}
+
+					// reordering widgets
+					if ($_GET.action == 'widgets-order') {
+						for (var prop in $_GET) {
+							if (prop.indexOf('sidebars') === 0) {
+								var widgets = $_GET[prop].split(',');
+								if (widgets.length > 0) {
+									for (var i in widgets) {
+										var widget = widgets[i].replace(/widget-\d+_/, '');
+										if (widget.indexOf('MiniMax_RichText-') === 0) {
+											dynamic_tinymce_init('widget-' + widget + '-text');
+										}
+									}
+								}
+							}
+						}
+					}
+				});
+
+			})(jQuery);
+		</script>
+		<?php
+	}
+
 }
+
+?>
